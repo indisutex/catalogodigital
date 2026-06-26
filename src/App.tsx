e151632c53f1fea28711abf9f485c58caf67c436
Geeko18 <internauta509@gmail.com>
2026-06-26 14:39:11 -0500

    fix: remover imports sin usar y aplicar layout app en principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Products from './pages/Products';
 import MenuDigital from './pages/MenuDigital';
 
 function App() {
